183b5df65ecedfb49a3064a77d60e5776453da4f
David Reyes <david.reyes.1994@hotmail.com>
2017-11-15 20:53:24 -0600

    vue files to handle messages added

---
+++ src/js/views/template/message.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+    <div :class="'message message-' + this.type">
+        <span :class="this.type">{{ message }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        timmer: null
+    },
+    props: ['type', 'message'],
+    methods: {
+        open: function() {
+            
+        },
+        close: function() {
+
+        }
+    },
+    mounted: function() {
+
+    }
+}
+</script>
+
+<style lang="css">
+.message {
+    width: 100px;
+    padding: 10px 20px;
+    border-radius: 5px;
+}
+
+.message .error {
+    background: red;
+    border: solid 3px red;
+}
+</style>
---
+++ src/js/views/template/messages.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+
+</template>
+
+<script>
+var navigation = require('./../../navigation.js');
+var Message = require('./message.js');
+export default {
+    components: {
+        'message': Message
+    },
+    methods: {
+        addMessage: function() {
+
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
